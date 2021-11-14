6013ff39c4040ac070ceb4b92721dfe3db665a96
lenycuk <93772056+lenycuk@users.noreply.github.com>
2021-11-14 22:23:58 +0300

    с ошибкой

--- task_3/solution.js
+++ task_3/solution.js
@@ -3,10 +3,10 @@ function renderCartItem(item) {
     const product = item
    
     // Задание №3.1. Формирование строки корзины
- product.id = 'id';
- product.title ='title';
- product.price = 'price';
- product.count = 'count';
+ //product.id = 'id';
+ //product.title ='title';
+ product.price = price;
+ product.count = count;
    
     let sum = count * price;
     let itemCountText = ' '+ count + `×`+ price + `₽ =`+ sum ;
