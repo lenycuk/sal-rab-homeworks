f54595389a7022753d5402186c0b5d8c109cdd61
lenycuk <93772056+lenycuk@users.noreply.github.com>
2021-11-14 19:27:37 +0300

    унифицированное решение

--- task_3/solution.js
+++ task_3/solution.js
@@ -3,24 +3,17 @@ function renderCartItem(item) {
     const product = item;
 
     // Задание №3.1. Формирование строки корзины
+    let id // идентификатор
+    let title // наименование
+    let price // цена
+    let count // количество
 
-    let product = {id: 1, title: 'Пицца', count: 5, price: '500.00'}; // product - объект вида {id: id, title: title, count: count, price: price}
-    // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
-    // id - идентификатор
-    // title - наименование
-    // price - цена
-    // count - количество
-
-    // сформируйте строку itemCountText вида:
-    // количество × цена ₽ = стоимость ₽
-    // например,
-    // 5 × 500.00 ₽ = 2500 ₽
-    let itemCountText = ``; // 1. Создайте переменную itemCountText, равную пустой строке
-    itemCountText = `${itemCountText} 5` ; // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
+    let itemCountText = ''; // 1. Создайте переменную itemCountText, равную пустой строке
+    itemCountText = `${itemCountText} count` ; // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
     itemCountText = `${itemCountText} ×`;  // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
-    itemCountText = `${itemCountText} 500.00`;// 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
+    itemCountText = `${itemCountText} price`;// 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
     itemCountText = `${itemCountText} ₽ =`;// 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
-    let sum = 5*500.00 // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
+    let sum = count * price // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
     itemCountText = `${itemCountText} sum `;// 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     itemCountText = `${itemCountText} ₽ `// 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 
