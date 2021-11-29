c28f9bde0c1bc2a0d41a6ecb0424f70c6746ac44
lenycuk <93772056+lenycuk@users.noreply.github.com>
2021-11-29 13:06:11 +0300

    автотест не проходит

--- task_3/solution.js
+++ task_3/solution.js
@@ -1,17 +1,13 @@
 
-function renderCartItem(item) {
-    const product = item
+   function renderCartItem(item) {
+    const product = item 
    
     // Задание №3.1. Формирование строки корзины
-
-let price;
-let count;
-
-product.count = count;
-product.price = price;
-    let sum = count * price;
-    let itemCountText = ' '+ count + `×`+ price + `₽ =`+ sum ;
+  
+    let itemCountText = `${product.count} × ${product.price} ₽ = ${product.count * product.price}`;
+   
     // Конец решения задания №3.1.
 
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
-}
+   }
+
