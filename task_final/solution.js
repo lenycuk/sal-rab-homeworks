b24f3b65493a9cdf877a51fb45640312e1c49c36
lenycuk <93772056+lenycuk@users.noreply.github.com>
2022-01-17 10:02:53 +0300

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -17,23 +17,26 @@
 //     - title - название позиции
 //     - count - количество в заказе
 
+
+
 function sendRequest(name, phone, address, goods, sum) {
     let data = {
         "client": name + ' ' + phone, 
         "order": {
-            "address": `улица ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`, 
+            "address": `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`, 
             "sum": sum,
         }, 
          "goods":[],
     };
     let countOfGoods = goods.length;
     for (let i = 0; i < countOfGoods; i += 1) {
-        let NEW = {
+        let new1 = {
             "title": goods[i].title,
             "count": goods[i].count 
         }
-        data.goods.push(NEW);
+        data.goods.push(new1);
     }
     let jsonData = JSON.stringify({data});
     return jsonData;
 }
+
