e56e82cafb56d197217e07776469e750de80cdd7
lenycuk <93772056+lenycuk@users.noreply.github.com>
2022-01-17 09:57:03 +0300

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -16,38 +16,24 @@
 // - goods: массив объектов с информацией о позициях заказа:
 //     - title - название позиции
 //     - count - количество в заказе
-// например:
-// {
-//    "data": {
-//      "client": "Иван +7(987)65-43-210",
-//      "order": {
-//        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
-//        "sum": 900
-//       },
-//       "goods": [
-//         {
-//           "title": "Пицца",
-//           "count": 2
-//         }
-//      ]
-//    }
-// }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {
+        "client": name + ' ' + phone, 
+        "order": {
+            "address": `улица ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`, 
+            "sum": sum,
+        }, 
+         "goods":[],
+    };
     let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        let NEW = {
+            "title": goods[i].title,
+            "count": goods[i].count 
+        }
+        data.goods.push(NEW);
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
+    let jsonData = JSON.stringify({data});
     return jsonData;
 }
